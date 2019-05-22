f4101cd6b2f992cecf8ad82382f9087cb4a61273
Marchello00 <nagov-mark@mail.ru>
2019-05-22 15:45:14 +0300

    Context managers tested and moved from debug to release

--- app/database.py
+++ app/database.py
@@ -3,7 +3,6 @@ import sqlalchemy.orm as sqlorm
 import sqlalchemy as sql
 from app import models
 from app.omdb_api import FilmOMDB
-from app import DEBUG
 
 
 class DB:
@@ -49,159 +48,80 @@ class DB:
 
     def film_in_db(self, film_id):
         film_id = str(film_id)
-        if DEBUG:
-            with self.connect() as session:
-                return bool(session.query(models.Film.imdbid).filter(
-                    models.Film.imdbid == film_id).all())
-        session = self.session_maker()
-        ret = bool(
-            session.query(models.Film.imdbid).filter(
+        with self.connect() as session:
+            return bool(session.query(models.Film.imdbid).filter(
                 models.Film.imdbid == film_id).all())
-        session.close()
-        return ret
 
     def film_in_chat_db(self, chat_id, film_id, favourite=None, watched=None):
         film_id = str(film_id)
-        if DEBUG:
-            with self.connect() as session:
-                query = session.query(models.ChatXFilm).filter(
-                    sql.and_(models.ChatXFilm.film_id == film_id,
-                             models.ChatXFilm.chat_id == chat_id))
-                if favourite is not None:
-                    query = query.filter(
-                        models.ChatXFilm.favourite == favourite)
-                if watched is not None:
-                    query = query.filter(models.ChatXFilm.watched == watched)
-                return bool(query.all())
-        session = self.session_maker()
-        query = session.query(models.ChatXFilm).filter(
-            sql.and_(models.ChatXFilm.film_id == film_id,
-                     models.ChatXFilm.chat_id == chat_id))
-        if favourite is not None:
-            query = query.filter(models.ChatXFilm.favourite == favourite)
-        if watched is not None:
-            query = query.filter(models.ChatXFilm.watched == watched)
-        ret = bool(query.all())
-        session.close()
-        return ret
+        with self.connect() as session:
+            query = session.query(models.ChatXFilm).filter(
+                sql.and_(models.ChatXFilm.film_id == film_id,
+                         models.ChatXFilm.chat_id == chat_id))
+            if favourite is not None:
+                query = query.filter(
+                    models.ChatXFilm.favourite == favourite)
+            if watched is not None:
+                query = query.filter(models.ChatXFilm.watched == watched)
+            return bool(query.all())
 
     def get_films_by_chat(self, chat_id, favourite=None, watched=None):
-        if DEBUG:
-            with self.connect() as session:
-                query = session.query(models.Film, models.ChatXFilm.watched,
-                                      models.ChatXFilm.favourite,
-                                      models.ChatXFilm.created_tm).filter(
-                    sql.and_(models.ChatXFilm.chat_id == chat_id,
-                             models.ChatXFilm.film_id ==
-                             models.Film.imdbid))
-                if favourite is not None:
-                    query = query.filter(models.ChatXFilm.favourite)
-                if watched is not None:
-                    query = query.filter(models.ChatXFilm.watched == watched)
-                return self.__film_from_query(query, inlib=True)
-        session = self.session_maker()
-        query = session.query(models.Film, models.ChatXFilm.watched,
-                              models.ChatXFilm.favourite,
-                              models.ChatXFilm.created_tm).filter(
-            sql.and_(models.ChatXFilm.chat_id == chat_id,
-                     models.ChatXFilm.film_id ==
-                     models.Film.imdbid))
-        if favourite is not None:
-            query = query.filter(models.ChatXFilm.favourite)
-        if watched is not None:
-            query = query.filter(models.ChatXFilm.watched == watched)
-        ret = self.__film_from_query(query, inlib=True)
-        session.close()
-        return ret
+        with self.connect() as session:
+            query = session.query(models.Film, models.ChatXFilm.watched,
+                                  models.ChatXFilm.favourite,
+                                  models.ChatXFilm.created_tm).filter(
+                sql.and_(models.ChatXFilm.chat_id == chat_id,
+                         models.ChatXFilm.film_id ==
+                         models.Film.imdbid))
+            if favourite is not None:
+                query = query.filter(models.ChatXFilm.favourite)
+            if watched is not None:
+                query = query.filter(models.ChatXFilm.watched == watched)
+            return self.__film_from_query(query, inlib=True)
 
     def insert_film(self, film):
-        if DEBUG:
-            with self.connect() as session:
-                if not self.film_in_db(film.imdbid):
-                    data = {key: str(value) for key, value in film.dct.items()
-                            if key in models.Film.__dict__ and
-                            not key.startswith('_') and
-                            not callable(key)}
-                    ins_film = models.Film(**data)
-                    session.add(ins_film)
-                return
-        session = self.session_maker()
-        if not self.film_in_db(film.imdbid):
-            data = {key: str(value) for key, value in film.dct.items()
-                    if key in models.Film.__dict__ and
-                    not key.startswith('_') and
-                    not callable(key)}
-            ins_film = models.Film(**data)
-            session.add(ins_film)
-            session.commit()
-        session.close()
+        with self.connect() as session:
+            if not self.film_in_db(film.imdbid):
+                data = {key: str(value) for key, value in film.dct.items()
+                        if key in models.Film.__dict__ and
+                        not key.startswith('_') and
+                        not callable(key)}
+                ins_film = models.Film(**data)
+                session.add(ins_film)
 
     def add_dependence(self, chat_id, film_id):
         film_id = str(film_id)
-        if DEBUG:
-            with self.connect() as session:
-                dep = models.ChatXFilm(chat_id=chat_id, film_id=film_id)
-                session.add(dep)
-                return
-        session = self.session_maker()
-        dep = models.ChatXFilm(chat_id=chat_id, film_id=film_id)
-        session.add(dep)
-        session.commit()
-        session.close()
+        with self.connect() as session:
+            dep = models.ChatXFilm(chat_id=chat_id, film_id=film_id)
+            session.add(dep)
+            return
 
     def del_dependence(self, chat_id, film_id):
         film_id = str(film_id)
-        if DEBUG:
-            with self.connect() as session:
-                dep = session.query(models.ChatXFilm).filter(
-                    sql.and_(models.ChatXFilm.film_id == film_id,
-                             models.ChatXFilm.chat_id == chat_id)).first()
-                if dep:
-                    session.delete(dep)
-                return
-        session = self.session_maker()
-        dep = session.query(models.ChatXFilm).filter(
-            sql.and_(models.ChatXFilm.film_id == film_id,
-                     models.ChatXFilm.chat_id == chat_id)).first()
-        if dep:
-            session.delete(dep)
-            session.commit()
-        session.close()
+        with self.connect() as session:
+            dep = session.query(models.ChatXFilm).filter(
+                sql.and_(models.ChatXFilm.film_id == film_id,
+                         models.ChatXFilm.chat_id == chat_id)).first()
+            if dep:
+                session.delete(dep)
+            return
 
     def set_favourite(self, chat_id, film_id, favourite):
         film_id = str(film_id)
-        if DEBUG:
-            with self.connect() as session:
-                film = session.query(models.ChatXFilm).filter(
-                    sql.and_(models.ChatXFilm.chat_id == chat_id,
-                             models.ChatXFilm.film_id == film_id)
-                ).first()
-                film.favourite = favourite
-                return
-        session = self.session_maker()
-        film = session.query(models.ChatXFilm).filter(
-            sql.and_(models.ChatXFilm.chat_id == chat_id,
-                     models.ChatXFilm.film_id == film_id)
-        ).first()
-        film.favourite = favourite
-        session.commit()
-        session.close()
+        with self.connect() as session:
+            film = session.query(models.ChatXFilm).filter(
+                sql.and_(models.ChatXFilm.chat_id == chat_id,
+                         models.ChatXFilm.film_id == film_id)
+            ).first()
+            film.favourite = favourite
+            return
 
     def set_watched(self, chat_id, film_id, watched):
         film_id = str(film_id)
-        if DEBUG:
-            with self.connect() as session:
-                film = session.query(models.ChatXFilm).filter(
-                    sql.and_(models.ChatXFilm.chat_id == chat_id,
-                             models.ChatXFilm.film_id == film_id)
-                ).first()
-                film.watched = watched
-                return
-        session = self.session_maker()
-        film = session.query(models.ChatXFilm).filter(
-            sql.and_(models.ChatXFilm.chat_id == chat_id,
-                     models.ChatXFilm.film_id == film_id)
-        ).first()
-        film.watched = watched
-        session.commit()
-        session.close()
+        with self.connect() as session:
+            film = session.query(models.ChatXFilm).filter(
+                sql.and_(models.ChatXFilm.chat_id == chat_id,
+                         models.ChatXFilm.film_id == film_id)
+            ).first()
+            film.watched = watched
+            return
